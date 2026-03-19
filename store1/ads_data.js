b0b7de6cbc682b792a31d745459bf70a6a4f3369
scev <minh.nn210582@sis.hust.edu.vn>
2026-03-20 02:16:15 +0700

    Switch to plain text manifest format
    - Replace banners.json with manifest.txt
    - Add ads_data.js (served from server, not generated on router)
    - Move images to images/ subdirectory

---
+++ store1/ads_data.js
@@ -0,0 +1,4 @@
+var ads = [
+  { image: "images/wifibanner1.jpg", url: "https://www.cat.edu.vn/", name: "banner1" },
+  { image: "images/wifibanner3.jpg", url: "https://huongnghiep.cat.edu.vn/", name: "banner3" }
+];
