efe9d54c4acb1bfba69c0a89e6b126bdc3d20eb0
scev <minh.nn210582@sis.hust.edu.vn>
2026-03-20 14:33:45 +0700

    test8

--- store1/ads_data.js
+++ store1/ads_data.js
@@ -5,23 +5,8 @@ var ads = [
     name: "banner1"
   },
   {
-    image: "images/wifibanner2.jpg",
-    url: "https://www.mahoataisan.com/",
+    image: "images/viec3mien2.png",
+    url: "https://viec3mien.vn",
     name: "banner2"
-  },
-  {
-    image: "images/wifibanner3.jpg",
-    url: "https://huongnghiep.cat.edu.vn/",
-    name: "banner3"
-  },
-  {
-    image: "images/wifibanner4.jpg",
-    url: "https://www.cat.edu.vn/",
-    name: "banner4"
-  },
-  {
-    image: "images/wifibanner5.jpg",
-    url: "https://www.v-sat.edu.vn/",
-    name: "banner5"
   }
 ];
