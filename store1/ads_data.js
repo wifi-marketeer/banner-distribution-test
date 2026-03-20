0fc9f2df76cfde2c8c981f17c52d9717c2deedd0
scev <minh.nn210582@sis.hust.edu.vn>
2026-03-20 14:08:12 +0700

    test5

--- store1/ads_data.js
+++ store1/ads_data.js
@@ -1,11 +1,11 @@
 var ads = [
   {
-    image: "images/wifibanner1.jpg",
+    image: "images/photo-1.jpg",
     url: "https://www.cat.edu.vn/",
     name: "banner1",
   },
   {
-    image: "images/wifibanner2.jpg",
+    image: "images/photo-2.jpg",
     url: "https://huongnghiep.cat.edu.vn/",
     name: "banner3",
   },
