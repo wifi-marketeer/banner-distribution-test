e427a032c8e5250d8d7a10ffa8ec2adaca1ebc80
scev <minh.nn210582@sis.hust.edu.vn>
2026-03-20 14:05:13 +0700

    test4

--- store1/ads_data.js
+++ store1/ads_data.js
@@ -1,11 +1,11 @@
 var ads = [
   {
-    image: "images/photo-1.jpg",
-    url: "https://viec3mien.vn",
+    image: "images/wifibanner1.jpg",
+    url: "https://www.cat.edu.vn/",
     name: "banner1",
   },
   {
-    image: "images/photo-2.jpg",
+    image: "images/wifibanner2.jpg",
     url: "https://huongnghiep.cat.edu.vn/",
     name: "banner3",
   },
